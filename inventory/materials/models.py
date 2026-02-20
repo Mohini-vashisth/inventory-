34477e32cc080dd6f4940c558e7087c74b0670b1
Mohini <mohini@Mohinis-MacBook-Air.local>
2026-02-20 21:38:09 +0530

    initial files

---
+++ inventory/materials/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Material(models.Model):
+    coil_no=models.CharField(max_length=10,primary_key=True)
+    date = models.DateField("reciept date ")
+    grade = models.CharField(max_length=10)
+    size = models.DecimalField(max_digits=10, decimal_places=3)
+    company = models.CharField(max_length=10)
+    vendor = models.CharField(max_length=50)
+    quantity= models.IntegerField()
+    heat_no = models.CharField(max_length=8)
+
