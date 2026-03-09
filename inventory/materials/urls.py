afe9b16b7988ff67b944639788cf451de949c428
Mohini <mohini@Mohinis-MacBook-Air.local>
2026-03-09 12:51:24 +0530

    added backend

--- inventory/materials/urls.py
+++ inventory/materials/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import index
 
-urlpatterns=[
-    path("", views.index)
+urlpatterns = [
+    path('', index, name='material_form'),
 ]
