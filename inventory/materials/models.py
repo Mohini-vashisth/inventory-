1c93a4e89f29ed6d25eb9e297950f4b5a1657a93
Mohini <mohini@Mohinis-MacBook-Air.local>
2026-03-21 11:55:34 +0530

    Added Excel import feature with null handling

--- inventory/materials/models.py
+++ inventory/materials/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Material(models.Model):
     coil_no=models.AutoField(primary_key=True)
-    date = models.DateField("reciept date ")
-    grade = models.CharField(max_length=10)
-    size = models.DecimalField(max_digits=10, decimal_places=3)
-    company = models.CharField(max_length=10)
-    vendor = models.CharField(max_length=50)
-    quantity= models.IntegerField()
-    heat_no = models.CharField(max_length=8)
+    date = models.DateField("reciept date ",null=True, blank=True)
+    grade = models.CharField(max_length=10 ,null=True, blank=True)
+    size = models.DecimalField(max_digits=10, decimal_places=3 ,null=True, blank=True)
+    company = models.CharField(max_length=10 ,null=True, blank=True)
+    vendor = models.CharField(max_length=50, null=True, blank=True)
+    quantity= models.IntegerField(null=True, blank=True)
+    heat_no = models.CharField(max_length=8,null=True, blank=True)
 
     def formatted_coil(self):
         return f"COIL{self.coil_no:04d}"
